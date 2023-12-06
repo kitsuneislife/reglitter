21038fb62093fc1bd2d9229d968cc1c641880db2
Glitch (subdued-glorious-pet) <none>
2023-12-06 11:36:39 +0000

    ♻️🌰 Updated with Glitch

--- Selfo/Tasks/Evorpg.js
+++ Selfo/Tasks/Evorpg.js
@@ -5,15 +5,15 @@ module.exports.run = (Client, channels) => {
   
   const channel = Client.channels.get(channels['evolusty']['play4'])
   
-  setTimeout(() => {
+  setInterval(() => {
     channel.send('.explorar')
-    channel.send('.minerar')
-    channel.send('.escavar')
-    channel.send('.caçar')
-    channel.send('.pescar')
+    setTimeout(() => { channel.send('.minerar') }, 1000)
+    setTimeout(() => { channel.send('.escavar') }, 2000)
+    setTimeout(() => { channel.send('.caçar')   }, 3000)
+    setTimeout(() => { channel.send('.pescar')  }, 4000)
   }, 31000)
   
-  setTimeout(() => {
+  setInterval(() => {
     channel.send('.caverna')
   }, 121000)
   
